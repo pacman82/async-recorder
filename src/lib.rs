40e065012c557816ebca493a8e3c50fe7aa648eb
Markus Klein <markus-klein@live.de>
2023-04-26 19:15:06 +0200

    Enable bulk insertions

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use std::fmt::Formatter;
 
 use async_trait::async_trait;
 use tokio::{
-    sync::mpsc::{unbounded_channel, UnboundedSender, UnboundedReceiver},
+    sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender},
     task::JoinHandle,
 };
 
@@ -35,7 +35,9 @@ where
     /// Sends the record to the internal actor for storage. This function will return immediatly.
     /// the record might only be persisted later.
     pub fn save(&self, record: T::Record) {
-        self.sender.send(Message(record)).expect("Receiver must not be closed.")
+        self.sender
+            .send(Message(record))
+            .expect("Receiver must not be closed.")
     }
 
     /// Stop the actor, deconstruct the recorder. Yields access to the underlying storage.
@@ -52,18 +54,30 @@ where
 /// Asynchronously spawned by [`Recorder`] in order to persist records
 struct Actor<T: Storage> {
     storage: T,
-    receiver: UnboundedReceiver<Message<T::Record>>
+    receiver: UnboundedReceiver<Message<T::Record>>,
 }
 
-impl<T> Actor<T> where T: Storage {
+impl<T> Actor<T>
+where
+    T: Storage,
+{
     pub fn new(storage: T, receiver: UnboundedReceiver<Message<T::Record>>) -> Self {
         Self { storage, receiver }
     }
 
     pub async fn run(mut self) -> T {
+        // If messages come in fast, we do not send them one by one, but rather collect all since
+        // the last call to save in one bulk;
+        let mut bulk = Vec::new();
         // Insert records until channel is closed.
         while let Some(Message(record)) = self.receiver.recv().await {
-            self.storage.save(record).await;
+            bulk.push(record);
+            // Push records into bulk, until it would block again.
+            while let Ok(Message(record)) = self.receiver.try_recv() {
+                bulk.push(record);
+            }
+            self.storage.save(&mut bulk).await;
+            bulk.clear();
         }
         self.storage
     }
@@ -86,7 +100,18 @@ pub trait Storage {
     /// Records saved in the storage
     type Record;
 
-    async fn save(&mut self, record: Self::Record);
+    /// Saves all the records to the persistence backend. Note that this method is infallible. This
+    /// implies that the responsibility of handling errors lies with the implementation of this
+    /// trait. So it is up to the implementation to decide how often to retry before (if ever)
+    /// giving up. What to log and so on.
+    ///
+    /// `records` contains all the records which are to be persisted with this call to save. The
+    /// records are passed in a `Vec` rather than in a single call to enable bulk insertion. They
+    /// are also passed in a `Vec` rather than a slice (`&[Record]`) in order to enable taking
+    /// ownership of each record and avoid cloning. Finally it is a `&mut Vec` rather than a buffer
+    /// so we can reuse it, without having to reallocate it a lot during the lifetime of our
+    /// application.
+    async fn save(&mut self, records: &mut Vec<Self::Record>);
 }
 
 /// This implementation is usefull for using as a fake for testing. In production you are more
@@ -98,7 +123,7 @@ where
 {
     type Record = T;
 
-    async fn save(&mut self, record: T) {
-        self.push(record);
+    async fn save(&mut self, records: &mut Vec<T>) {
+        self.append(records);
     }
 }
--- tests/integration.rs
+++ tests/integration.rs
@@ -1,4 +1,8 @@
-use async_recorder::Recorder;
+use std::{mem::swap, sync::Arc};
+
+use async_recorder::{Recorder, Storage};
+use async_trait::async_trait;
+use tokio::sync::Mutex;
 
 #[tokio::test]
 async fn record_event_to_persistence_backend() {
@@ -10,4 +14,49 @@ async fn record_event_to_persistence_backend() {
     let storage = recorder.into_storage().await;
 
     assert_eq!(["Hello, World!"].as_slice(), storage);
+}
+
+#[tokio::test]
+async fn persist_events_in_bulk() {
+    let bulks = Arc::new(Mutex::new(Vec::new()));
+    let storage = StorageSpy::new(bulks.clone());
+
+    let recorder = Recorder::new(storage).await;
+    {
+        // Keep guard to bulks, so spy can not persist until it is cleared
+        let _guard = bulks.lock().await;
+        recorder.save("first");
+        recorder.save("second");
+    }
+    // Now the guard has been freed. Storage can now persist immediatly. Wait for this to be
+    // fininshed.
+    let _ = recorder.into_storage().await;
+
+    // Verify that there is one bulk with two entries.
+    let bulks = bulks.lock().await;
+    assert_eq!(1, bulks.len());
+    assert_eq!(["first", "second"].as_slice(), bulks[0])
+}
+
+/// Makes a copy of each received bulk.
+struct StorageSpy<T> {
+    /// Make this Arc Mutex, so we can block saving and observe bulk behaviour
+    bulks: Arc<Mutex<Vec<Vec<T>>>>,   
+}
+
+impl<T> StorageSpy<T> {
+    fn new(bulks: Arc<Mutex<Vec<Vec<T>>>>) -> Self {
+        StorageSpy { bulks }
+    }
+}
+
+#[async_trait]
+impl<T> Storage for StorageSpy<T> where T: Send {
+    type Record = T;
+
+    async fn save(&mut self, records: &mut Vec<T>) {
+        let mut tmp = Vec::new();
+        swap(&mut tmp, records);
+        self.bulks.lock().await.push(tmp);
+    }
 }
